b813a90496031486d02975922320786d9551a207
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-05-17 01:07:03 +0330

    Add base of ParsPay class

---
+++ parspay/parspay.py
@@ -0,0 +1,12 @@
+from typing import Any, Callable, Union
+from .unified import unified
+
+class ParsPay:
+    def __init__(self, gateway: Any):
+        self.gateway = gateway
+
+    def __getattr__(self, item: str) -> Union[Callable[..., Any], Any]:
+        attr = getattr(self.gateway, item)
+        if callable(attr):
+            return unified(attr)
+        return attr
