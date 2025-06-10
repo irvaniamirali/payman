399b3b4d391047faa71035a167a6d822947f5e5a
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-06-11 00:22:14 +0330

    add payment, verification, and revenue share models to public API

--- payman/gateways/zarinpal/__init__.py
+++ payman/gateways/zarinpal/__init__.py
@@ -1,10 +1,11 @@
 from .gateway import ZarinPal
-from .models import (
-    PaymentMetadata,
-    PaymentResponse,
-    PaymentVerify,
-    Payment,
-    PaymentData,
-    VerifyPaymentResponse,
-    CallbackParams
-)
+from .models import CallbackParams
+from .models import PaymentRequest, PaymentMetadata
+from .models import PaymentResponse, PaymentData
+from .models import PaymentVerifyRequest
+from .models import PaymentVerifyResponse
+from .models import RevenueShareRequest
+from .models import RevenueShareResponse
+from .models import RevenueShareVerifyRequest
+from .models import RevenueShareVerifyResponse
+from .models import Wage
