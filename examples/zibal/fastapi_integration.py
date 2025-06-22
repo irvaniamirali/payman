416a845ee849dcab9681ac9afbc527dfad4773da
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-06-23 01:19:57 +0330

    Update fastapi_integration.py

--- examples/zibal/fastapi_integration.py
+++ examples/zibal/fastapi_integration.py
@@ -8,17 +8,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.exceptions import HTTPException
 
-from payman import ZarinPal
+from payman import Zibal
 from payman.errors.base import PaymentGatewayError
-from payman.gateways.zarinpal.models import (
+from payman.gateways.zibal.models import (
     PaymentRequest,
     PaymentResponse,
     PaymentVerifyRequest,
     PaymentVerifyResponse
 )
 
-# Initialize ZarinPal client
-pay = ZarinPal(merchant_id="12345678-1234-1234-1234-123456789012", sandbox=True)
+# Initialize Zibal client
+pay = Zibal(merchant="zibal")
 
 app = FastAPI()
 
@@ -28,7 +28,6 @@ async def error_handling_middleware(request: Request, call_next):
         response = await call_next(request)
         return response
     except Exception as e:
-        print('\n\n\n\n', e, '\n\n\n')
         if isinstance(e, PaymentGatewayError):
             raise HTTPException(status_code=400, detail=str(e))
         raise HTTPException(status_code=500, detail="Internal Server Error")
@@ -38,8 +37,8 @@ async def initiate_payment(request: PaymentRequest):
     return await pay.payment(request)
 
 @app.get("/redirect-to-payment-page/{authority}")
-async def redirect_to_payment_page(authority: str):
-    redirect_url = pay.payment_url_generator(authority)
+async def redirect_to_payment_page(track_id: int):
+    redirect_url = pay.payment_url_generator(track_id)
     return RedirectResponse(redirect_url)
 
 @app.post("/verify-payment", response_model=PaymentVerifyResponse)
