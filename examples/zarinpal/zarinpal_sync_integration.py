1c0a83074c913c0f8be76e8e2994a3db5d738fdb
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-06-23 13:42:57 +0330

    Refactor zarinpal_sync_integration.py

--- examples/zarinpal/zarinpal_sync_integration.py
+++ examples/zarinpal/zarinpal_sync_integration.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add project root to sys.path for local imports
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
 import time
 from payman import ZarinPal
 from payman.gateways.zarinpal.models import PaymentRequest, PaymentMetadata, PaymentVerifyRequest
