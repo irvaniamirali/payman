1459bdd7f51618c6f15452989bc5c2d2ce87fe1c
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-06-23 13:44:06 +0330

    Refactore zibal code examples

--- examples/zibal/fastapi_integration.py
+++ examples/zibal/fastapi_integration.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add project root to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.exceptions import HTTPException
--- examples/zibal/zibal_sync_integration.py
+++ examples/zibal/zibal_sync_integration.py
@@ -1,12 +1,6 @@
-import sys
-import os
-
-# Add project root to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
+import time
 from payman import Zibal
 from payman.gateways.zibal.models import PaymentRequest, PaymentVerifyRequest, CallbackParams
-import time
 
 # Initialize Zibal client with your merchant ID
 pay = Zibal(merchant="zibal")  # sandbox mode
