d4853321b4ba4247e3fd41e1890713c0e32eff5e
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-06-26 23:40:34 +0330

    add new request and response pydantic models

--- payman/gateways/zarinpal/__init__.py
+++ payman/gateways/zarinpal/__init__.py
@@ -1,7 +1,14 @@
 from .gateway import ZarinPal
-from .models import CallbackParams
-from .models import PaymentRequest, PaymentMetadata, PaymentResponse
-from .models import VerifyRequest, VerifyResponse
-from .models import ReverseRequest, ReverseResponse
-from .models import UnverifiedTransaction, UnverifiedPayments
-from .models import Wage
+from .models import (
+    CallbackParams,
+    PaymentMetadata,
+    PaymentRequest,
+    PaymentResponse,
+    VerifyRequest,
+    VerifyResponse,
+    ReverseRequest,
+    ReverseResponse,
+    UnverifiedTransaction,
+    UnverifiedPayments,
+    Wage
+)
