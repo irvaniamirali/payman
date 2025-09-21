3e3a4a4848e1c5b25c906def9713d6336f027a96
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-21 08:54:10 +0330

    refactor(core): reorganize core modules and gateways
    - Moved errors to core/exceptions
    - Moved http client to core/http
    - Removed old gateways folder (zarinpal & zibal)

--- payman/gateways/zibal/components/error_handler.py
+++
@@ -1,9 +0,0 @@
-from payman.errors import GatewayManager
-
-
-class ErrorHandler:
-    def handle(self, response: dict):
-        if response.get("result") != 100:
-            raise GatewayManager.handle_error(
-                "Zibal", response.get("result"), response.get("message")
-            )
